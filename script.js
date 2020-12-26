1573aed20905f438b8430db1d55927727249513d
jotabraham <jotabraham@gmail.com>
2020-12-26 18:05:21 -0500

    fixed func compareGuesses

--- script.js
+++ script.js
@@ -2,15 +2,26 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
-
+// Generates number to be guessed
 const generateTarget = () => Math.floor(Math.random() * 10);
+
+// Compares guesses and returns boolean value
 const compareGuesses = (userChoice, compChoice, num) => {
-    let userDiff = Math.abs(num, userChoice);
-    let compDiff = Math.abs(num, compChoice);
+    let userDiff = Math.abs(num - userChoice);
+    let compDiff = Math.abs(num - compChoice);
     if (userDiff === compDiff || compDiff > userDiff) {
         return true;
     } else if (compDiff < userDiff) {
         return false;
     }
 };
+
+console.log(generateTarget());
+console.log(compareGuesses(1,1,1));
+console.log(compareGuesses(3,9,4));
+console.log(compareGuesses(9,3,1));
+
+// Updates scores
+const updateScore = winner => {
+
+}
