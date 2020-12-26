9cdb8a87491ae62b9a7c0534832bc5ef53c089cb
jotabraham <jotabraham@gmail.com>
2020-12-26 17:52:39 -0500

    edit js

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ let currentRoundNumber = 1;
 // Write your code below:
 
 const generateTarget = () => Math.floor(Math.random() * 10);
-const compareGuesses = (userGuess, compGuess, num) => {
-    
+const compareGuesses = (userChoice, compChoice, num) => {
+    let userDiff = Math.abs(num, userChoice);
+    let compDiff = Math.abs(num, compChoice);
+    if (userDiff === compDiff || compDiff > userDiff) {
+        return true;
+    } else if (compDiff < userDiff) {
+        return false;
+    }
 };
